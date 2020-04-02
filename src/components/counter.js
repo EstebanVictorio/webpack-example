cf4d9a414f55600f915cbd95bbdae48f44413b8e
Esteban Victorio <estebanvictorio92@gmail.com>
2020-04-01 23:16:10 -0600

    [add] add emotion

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,10 +1,22 @@
+import styled from "@emotion/styled"
+
+
+const StyledCounter = styled.div`
+  background-color: brown;
+  padding: 25px;
+  .counter {
+    border: 5px solid red;
+    color: red;
+    font-size: 24px;
+  }
+`
+
 const Counter = () => {
   return (
-    <div>
-      <input type="number"/>
-    </div>
+    <StyledCounter isBlue >
+      <input className="counter" type="number"/>
+    </StyledCounter>
   )
 }
-
 
 export default Counter
--- src/hello-world.js
+++ src/hello-world.js
@@ -1,8 +1,16 @@
 import Home from "pages/home"
+import { Global, css } from "@emotion/core"
 import ConfirmPayment from "pages/user/confirm-payment"
 import Counter from "components/counter"
 const HelloWorld = () => (
   <div>
+    <Global css={
+      css`
+        body {
+          margin: 0;
+        }
+      `
+    }/>
     Hello world from React!
   <Home />
   <ConfirmPayment />
